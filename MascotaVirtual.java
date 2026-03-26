493062f8690bcfdbdbaded58033a9caf724c614a
johesmy ruiz <johesmyruiz39@gmail.com>
2026-03-26 08:51:56 -0600

    agregue MascotaVirtual

---
+++ MascotaVirtual.java
@@ -0,0 +1,10 @@
+class MascotaVirtual {
+    private Integer nombre;
+    private Integer genergia;
+    public  String color; 
+                       
+    public Viod ladrar () {
+    }
+    public Viod jugar () {    
+    }
+}
